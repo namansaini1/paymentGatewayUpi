f28c237f286b02ed05ba495bb4e5d1b76d7b2371
namansaini1 <54397410+namansaini1@users.noreply.github.com>
2025-06-23 23:58:33 +0530

    submit payment info

--- submitpayment.php
+++ submitpayment.php
@@ -2,6 +2,9 @@
 if (isset($_POST["amount"])) {
     $amount = $_POST["amount"];
     $name = $_POST["name"];
+    $email = $_POST["email"];
+    $mobile = $_POST["mobile"];
+    $product_name = $_POST["product_name"];
     
     $callback_url = "https://yourdomain.com/success.php"; // your callback url
     $number=mt_rand(1000000000, 9999999999); // generate a random number for client_txn_id
@@ -9,10 +12,10 @@ if (isset($_POST["amount"])) {
         'key' => "YOUR_API_KEY", // replace with your live API KEY
         'client_txn_id' => "$number",
         'amount' => "$amount",
-        'p_info'=>"Product Name",
-        'customer_name'=>"Tutorials Website",
-        'customer_email'=>"tutorialswebsite@gmail.com",
-        "customer_mobile"=>"9876543210",
+        'p_info'=>"$product_name",
+        'customer_name'=>"$name",
+        'customer_email'=>"$email",
+        "customer_mobile"=>"$mobile",
         'redirect_url'=>"$callback_url" 
     );
     $jsonpaymentdata = json_encode($payload_data);
